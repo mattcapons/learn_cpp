5ec594596983c20a6dda49b0c1c09c338b99bdb6
Matteo Capone <matteo.capone11@gmail.com>
2026-03-08 09:56:58 +0100

    learned I/O

---
+++ firstinput.cpp
@@ -0,0 +1,12 @@
+// firstinput.cpp
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age;
+
+    cout << "What is your age? ";
+    cin >> age;
+    cout << "Your age is " << age << endl;
+}
--- hello.cpp
+++ hello.cpp
@@ -3,3 +3,8 @@
 // By Matteo Capone
 
 #include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "Hello, world!\n";
+}
